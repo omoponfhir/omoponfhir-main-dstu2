70ee0aeaa1f4e51da7f03a35bc02a29da3f149c2
Myung Choi <myungchoi@gmail.com>
2020-11-19 02:17:17 -0500

    entity column names fixed

--- updateSubmodules.sh
+++ updateSubmodules.sh
@@ -1,1 +1,3 @@
-git submodule foreach git pull origin
+git submodule update --remote -- omoponfhir-omopv5-jpabase
+git submodule update --remote -- omoponfhir-omopv5-dstu2-mapping
+git submodule update --remote -- omoponfhir-dstu2-server
